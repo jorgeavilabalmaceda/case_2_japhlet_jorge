549f122e5de8316fa3d6a4e19e2a0695c0bbc670
JaphletM <grace.mputeni@hotmail.co.uk>
2026-09-22 15:35:12 +0200

    updates

--- Data_Analysis.R
+++ Data_Analysis.R
@@ -158,6 +158,60 @@ FEDd0
 
 #p-value 0.14> 0.05 so we fail to reject the null. The series CPI has a unit root at I(1) 
 
+#Robustness Checking unit root tests
+
+#Industrial Production. Set d=2
+
+IPd2_check<-adf(diff(diff(log_industrial_production)), deterministics = "intercept")
+IPd2_check
+
+#p-value is 1e-04<0.05 so we reject the null
+#Set d=1 and test again.
+IPd1_check<-adf(diff(log_industrial_production), deterministics = "intercept")
+IPd1_check
+
+#p-value is 9.347e-09<0.05 so we reject the null
+#Set d=0 and test again.
+
+IPd0_check<-adf(log_industrial_production, deterministics = "trend")
+IPd0_check
+#p-value is 0.6887 > 0.05 so we fail to reject the null. The log(IP) series has a unit root at I(1)
+
+
+#Unit Root Test - Consumer Price Index
+#Set d=2
+CPId2_check <-adf(diff(diff(log_consumer_price_index)), deterministics = "intercept")
+CPId2_check
+
+#p-value is 8.175e-13<0.05 so we reject the null.
+#set d=1 and test again
+
+CPId1_check <-adf(diff(log_consumer_price_index), deterministics = "intercept")
+CPId1_check
+#p-value is 0.06725>0.05 so we fail to reject the null. -> Delta^2 log(CPI) series has a unit root at I(2)
+
+
+CPId0_check <-adf(log_consumer_price_index,deterministics = "trend")
+CPId0_check
+#p-value is  0.9127 >0.05 so we fail to reject the null -> series has a unit root at I(1)
+
+#Unit Root Test - Fed Funds Rate 
+#Set d=2
+
+FEDd2_check<-adf(diff(diff(federal_funds_rate)), deterministics = "intercept")
+FEDd2_check
+#p-value 4.023e-24< 0.05 to reject the null 
+#Set d=1 and test again.
+
+FEDd1_check<-adf(diff(federal_funds_rate), deterministics = "intercept")
+FEDd1_check
+
+#p-value 9.201e-12 < 0.05 to reject the null 
+#Set d=0 and test again.
+FEDd0_check <-adf(federal_funds_rate, deterministics = "trend")
+FEDd0_check
+
+#p-value  0.1017> 0.05 so we fail to reject the null. The series CPI has a unit root at I(1) 
 
 
 #Constructing the VAR model 
